4b72e0d230a80eac6a8e1ea4dc68b7f283cf9a1f
Josh Harris <49429928+jharris711@users.noreply.github.com>
2024-01-21 19:36:00 -0500

    Create root/auth layoutes - Add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import { Routes, Route } from 'react-router-dom';
+
+import { Home } from './_root/pages';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
+import SigninForm from './_auth/forms/SignInForm';
+import SignUpForm from './_auth/forms/SignUpForm';
+
 import './globals.css';
 
 function App() {
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline'>Hello Snapgram!</h1>
-    </div>
+    <main className='flex h-screen'>
+      <Routes>
+        {/* Public Routes */}
+        <Route element={<AuthLayout />}>
+          <Route path='/sign-in' element={<SigninForm />} />
+          <Route path='/sign-up' element={<SignUpForm />} />
+        </Route>
+        {/* Private Routes */}
+        <Route element={<RootLayout />}></Route>
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
   );
 }
 
