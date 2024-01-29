a00ffdcb2e7d14048d75e443948cb43786b0d3aa
Josh Harris <49429928+jharris711@users.noreply.github.com>
2024-01-29 01:01:12 -0500

    Add create post functionality

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,11 +1,12 @@
 import { ID, Query } from 'appwrite';
 
-import type { INewUser } from '@/types';
-import { appwriteConfig, account, avatars, databases } from './config';
+import { appwriteConfig, account, databases, storage, avatars } from './config';
+import { IUpdatePost, INewPost, INewUser, IUpdateUser } from '@/types';
 
-/**
- * Sign up new user
- */
+/* -------------------------------------------------------------------------- */
+/*                                    AUTH                                    */
+/* -------------------------------------------------------------------------- */
+/* --------------------------------- SIGN UP -------------------------------- */
 export async function createUserAccount(user: INewUser) {
   try {
     const newAccount = await account.create(
@@ -29,14 +30,12 @@ export async function createUserAccount(user: INewUser) {
 
     return newUser;
   } catch (error) {
-    console.error(error);
+    console.log(error);
     return error;
   }
 }
 
-/**
- * Save user to database
- */
+/* ----------------------------- SAVE USER TO DB ---------------------------- */
 export async function saveUserToDB(user: {
   accountId: string;
   email: string;
@@ -54,29 +53,36 @@ export async function saveUserToDB(user: {
 
     return newUser;
   } catch (error) {
-    console.error(error);
+    console.log(error);
   }
 }
 
-/**
- * Sign in user
- */
+/* --------------------------------- SIGN IN -------------------------------- */
 export async function signInAccount(user: { email: string; password: string }) {
   try {
     const session = await account.createEmailSession(user.email, user.password);
 
     return session;
   } catch (error) {
-    console.error(error);
+    console.log(error);
   }
 }
 
-/**
- * Get current user
- */
-export async function getCurrentUser() {
+/* ------------------------------- GET ACCOUNT ------------------------------ */
+export async function getAccount() {
   try {
     const currentAccount = await account.get();
+
+    return currentAccount;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+/* -------------------------------- GET USER -------------------------------- */
+export async function getCurrentUser() {
+  try {
+    const currentAccount = await getAccount();
 
     if (!currentAccount) throw Error;
 
@@ -90,16 +96,123 @@ export async function getCurrentUser() {
 
     return currentUser.documents[0];
   } catch (error) {
-    console.error(error);
+    console.log(error);
+    return null;
   }
 }
 
+/* -------------------------------- SIGN OUT -------------------------------- */
 export async function signOutAccount() {
   try {
     const session = await account.deleteSession('current');
 
     return session;
   } catch (error) {
-    console.error(error);
+    console.log(error);
   }
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                    POSTS                                   */
+/* -------------------------------------------------------------------------- */
+/* ------------------------------- CREATE POST ------------------------------ */
+export async function createPost(post: INewPost) {
+  try {
+    // Upload file to appwrite storage
+    const uploadedFile = await uploadFile(post.file[0]);
+
+    if (!uploadedFile) throw Error;
+
+    // Get file url
+    const fileUrl = getFilePreview(uploadedFile.$id);
+    if (!fileUrl) {
+      await deleteFile(uploadedFile.$id);
+      throw Error;
+    }
+
+    // Convert tags into array
+    const tags = post.tags?.replace(/ /g, '').split(',') || [];
+
+    // Create post
+    const newPost = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.postCollectionId,
+      ID.unique(),
+      {
+        creator: post.userId,
+        caption: post.caption,
+        imageUrl: fileUrl,
+        imageId: uploadedFile.$id,
+        location: post.location,
+        tags: tags,
+      }
+    );
+
+    if (!newPost) {
+      await deleteFile(uploadedFile.$id);
+      throw Error;
+    }
+
+    return newPost;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+/* ------------------------------- UPLOAD FILE ------------------------------ */
+export async function uploadFile(file: File) {
+  try {
+    const uploadedFile = await storage.createFile(
+      appwriteConfig.storageId,
+      ID.unique(),
+      file
+    );
+
+    return uploadedFile;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+/* ------------------------------ GET FILE URL ------------------------------ */
+export function getFilePreview(fileId: string) {
+  try {
+    const fileUrl = storage.getFilePreview(
+      appwriteConfig.storageId,
+      fileId,
+      2000,
+      2000,
+      'top',
+      100
+    );
+
+    if (!fileUrl) throw Error;
+
+    return fileUrl;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+/* ------------------------------- DELETE FILE ------------------------------ */
+export async function deleteFile(fileId: string) {
+  try {
+    await storage.deleteFile(appwriteConfig.storageId, fileId);
+
+    return { status: 'ok' };
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getRecentPosts() {
+  const posts = await databases.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.postCollectionId,
+    [Query.orderDesc('$createdAt'), Query.limit(20)]
+  );
+
+  if (!posts) throw Error;
+
+  return posts;
 }
--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -6,13 +6,20 @@ import {
 } from '@tanstack/react-query';
 
 import {
+  createPost,
   createUserAccount,
+  getRecentPosts,
   signInAccount,
   signOutAccount,
 } from '../appwrite/api';
 
-import type { INewUser } from '@/types';
+import type { INewPost, INewUser } from '@/types';
 
+import { QUERY_KEYS } from './queryKeys';
+
+/* -------------------------------------------------------------------------- */
+/*                                    AUTH                                    */
+/* -------------------------------------------------------------------------- */
 export const useCreateUserAccount = () => {
   return useMutation({
     mutationFn: (user: INewUser) => createUserAccount(user),
@@ -29,5 +36,27 @@ export const useSignInAccount = () => {
 export const useSignOutAccount = () => {
   return useMutation({
     mutationFn: signOutAccount,
+  });
+};
+
+/* -------------------------------------------------------------------------- */
+/*                                    POSTS                                   */
+/* -------------------------------------------------------------------------- */
+export const useCreatePost = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (post: INewPost) => createPost(post),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      });
+    },
+  });
+};
+
+export const useGetRecentPosts = () => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+    queryFn: getRecentPosts,
   });
 };
